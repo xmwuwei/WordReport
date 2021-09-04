f69c83cd6d95ace50dfac76ef0c326a9631d6822
wei.wu <wei.wu01@sap.com>
2021-09-04 20:35:15 +0800

    Code refined

--- WordReport/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WordReport
-{
-    using System;
-    using System.Runtime.CompilerServices;
-
-    internal delegate void ShowProgressEvent(ReportStatus reportStatus, int nProgressStep);
-}
--- WordReport/EventDefinations.cs
+++ WordReport/EventDefinations.cs
@@ -4,4 +4,6 @@
     using System.Runtime.CompilerServices;
 
     public delegate bool EnumWindowsEvent(IntPtr hWnd, int y);
+    public delegate void ShowProgressEvent(ReportStatus reportStatus, int nProgressStep);
+
 }
